3b1164a4b06563398253d42d373987c461ecf6df
LyyHua <lyhuavanly@gmail.com>
2025-01-11 02:01:41 +0700

    This shit is pure http request. There's nothing microservice here (tho only for the mean time)

---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,28 @@
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionSvcHttpClient(HttpClient httpClient, IConfiguration config)
+{
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(x => x.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+
+        var auctionURL = config["AuctionServiceUrl"];
+
+        var url = auctionURL + "/api/auctions";
+
+        if (!string.IsNullOrEmpty(lastUpdated))
+        {
+            url += $"?date={lastUpdated}";
+        }
+
+        var items = await httpClient.GetFromJsonAsync<List<Item>>(url);
+
+        return items ?? [];
+    }
+}
