b40b5e2f3afd238b8296788185b1a439f3b338f1
LyyHua <lyhuavanly@gmail.com>
2025-01-25 02:15:33 +0700

    I know it's 2a.m but since i need to finish a section a day so yeah kinda have to grind through it. However since this is still the front or the client i'm very enjoying it so fuck off.

--- frontend/web-app/app/nav/NavBar.tsx
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -1,12 +1,16 @@
 import Search from "./Search"
 import Logo from "./Logo"
+import LoginButton from "./LoginButton"
+import UserActions from "./UserActions"
+import { getCurrentUser } from "../actions/authActions"
 
-export default function NavBar() {
+export default async function NavBar() {
+    const user = await getCurrentUser()
     return (
         <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md">
             <Logo />
             <Search />
-            <div>Login</div>
+            {user ? <UserActions user={user}/> : <LoginButton />}
         </header>
     )
 }
