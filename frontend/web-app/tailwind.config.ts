0a91127ffc8434f6b4177f9234b1bbaeb5ddf867
LyyHua <lyhuavanly@gmail.com>
2025-01-24 13:12:46 +0700

    Finally make the client side (although in the moment it only able to read filter order by and stuff not all of them POST and PUT and auth stuff) however it sure feel good to code in web especially Nextjs. Shit feel like home

---
+++ frontend/web-app/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+import { plugin, content } from "flowbite-react/tailwind";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    content(),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [plugin()],
+} satisfies Config;
